9d690f9837df2c7437c121142fc432c9ab2a4edf
karthik1172 <karthik.p.rashinkar@gmail.com>
2024-07-18 12:39:15 +0530

    Initial Commit

---
+++ MoviesSwiftUI/MoviesSwiftUIApp.swift
@@ -0,0 +1,13 @@
+
+import SwiftUI
+
+@main
+struct MoviesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                ContentView()
+            }
+        }
+    }
+}
